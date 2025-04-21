c7d121d0f539e3ce8854f8546a17ca3bcf33dacb
Muspi Merol <me@promplate.dev>
2025-04-22 01:05:17 +0800

    add metadata extraction api

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from traceback import format_exc
 from fastapi import Body, FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse, RedirectResponse
 
-from src.api import pdf_to_markdown
+from src.api import Metadata, parse_pdf_metadata, pdf_to_markdown
 from src.utils.llm import console
 from src.utils.response import make_streaming_response
 
@@ -24,6 +24,15 @@ async def convert_pdf_to_markdown(pdf: bytes = Body(media_type="application/pdf"
             return await make_streaming_response(await pdf_to_markdown(BytesIO(pdf), stream=True), media_type="text/markdown")
         else:
             return PlainTextResponse(await pdf_to_markdown(BytesIO(pdf)), media_type="text/markdown")
+    except Exception as e:
+        console.print("\n" + indent(format_exc().strip(), " ") + "\n", style="red")
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+
+@app.post("/extract", response_model=Metadata)
+async def parse_metadata_from_pdf(pdf: bytes = Body(media_type="application/pdf")):
+    try:
+        return await parse_pdf_metadata(BytesIO(pdf))
     except Exception as e:
         console.print("\n" + indent(format_exc().strip(), " ") + "\n", style="red")
         raise HTTPException(status_code=500, detail=str(e)) from e
--- src/api.py
+++ src/api.py
@@ -1,10 +1,11 @@
 from collections.abc import AsyncIterable
-from typing import Literal, overload
+from typing import Literal, TypedDict, overload
 
 from pdfminer.high_level import extract_text
 from promplate import parse_chat_markup
+from pydantic import TypeAdapter
 
-from .templates import pdf2md
+from .templates import extract, pdf2md
 from .utils.llm import complete, generate
 
 
@@ -22,5 +23,18 @@ async def pdf_to_markdown(pdf_path, stream=False):
     messages = parse_chat_markup(pdf2md.render({"content": text}))
 
     if stream:
-        return generate(messages, temperature=0, model="gpt-4o-mini")
-    return await complete(messages, temperature=0, model="gpt-4o-mini")
+        return generate(messages, temperature=0, model="grok-2-1212")
+    return await complete(messages, temperature=0, model="grok-2-1212")
+
+
+Metadata = TypedDict("Metadata", {"title": str, "abstract": str, "keywords": list[str], "success": Literal[True]}) | TypedDict("Error", {"success": Literal[False]})
+validator = TypeAdapter(Metadata)
+
+
+async def parse_pdf_metadata(pdf_path):
+    text = extract_text(pdf_path)
+
+    messages = parse_chat_markup(extract.render({"content": text}))
+
+    res = await complete(messages, temperature=0, model="grok-2-1212", response_format={"type": "json_object"})
+    return validator.validate_json(res)
