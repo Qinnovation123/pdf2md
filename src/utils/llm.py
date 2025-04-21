1581749983dd59d3f8d7f05c15675a9e3d42f8dd
Muspi Merol <me@promplate.dev>
2025-04-22 00:43:09 +0800

    improve debug printing

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -1,3 +1,5 @@
+from asyncio import Lock
+
 from promplate.llm.openai import AsyncChatGenerate
 from promplate.prompt.chat import ensure
 from promplate_trace.auto import patch
@@ -12,8 +14,42 @@ generate = patch.chat.agenerate(AsyncChatGenerate())
 console = Console(markup=False)
 
 
-async def complete(prompt, /, **kwargs):
+def fix_json(json: str):
+    from partial_json_parser import ensure_json
+
+    return ensure_json(json)
+
+
+lock = Lock()
+
+
+async def _debug_complete(prompt, /, **kwargs):
+    res = ""
+    from rich.live import Live
+
+    if is_json := kwargs.get("response_format", {}).get("type", "text") != "text":
+        from rich.json import JSON as Render  # noqa: N811
+    else:
+        from rich.markdown import Markdown as Render
+
+    async with lock:
+        with Live(vertical_overflow="visible") as live:
+            async for i in generate(prompt, **kwargs):
+                if i:
+                    res += i
+                    live.update(Render(fix_json(res) if is_json else res), refresh=True)
+
+        print()
+        print_token_usage(prompt, res)
+
+    return res
+
+
+async def complete(prompt, /, pretty=__debug__, **kwargs):
     show_prompt(ensure(prompt))  # type: ignore
+
+    if pretty:
+        return await _debug_complete(prompt, **kwargs)
 
     res = ""
     async for i in generate(prompt, **kwargs):
